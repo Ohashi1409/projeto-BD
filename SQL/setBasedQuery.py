0287a1d04b7f744cab73e4e3fb68de88bf1aedf9
João Ohashi <131549440+Ohashi1409@users.noreply.github.com>
2025-07-15 20:53:57 -0300

    Consultas baseadas em conjunto (Union/Intersect/Except) concluídas

---
+++ SQL/setBasedQuery.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="rootpassword",
+    database="codeforces-db"
+)
+
+cursor = conn.cursor()
+
+print("===Usuários que são criadores de contests ou grupos===")
+query = """
+    SELECT id, user_name, email
+    FROM user_table
+    WHERE id in (
+        SELECT user_id_creator 
+        FROM group_table 
+        UNION
+        SELECT user_id_creator
+        FROM contest_table
+    )
+"""
+
+cursor.execute(query)
+results = cursor.fetchall()
+
+for rows in results:
+    print(f"ID: {rows[0]}, Nome: {rows[1]}, Email: {rows[2]}") 
+
+print(f"\nTotal: {len(results)} usuários")
+
+cursor.close()
+conn.close()
